da95f247f6ffb1028f16386e6e6b5ad9bd346293
Sing Mak <mssjsg@gmail.com>
2020-11-01 19:14:11 +0800

    build site

--- _site/assets/js/search-data.js
+++ _site/assets/js/search-data.js
@@ -1,1 +1,6 @@
-var store = []
+var store = [{
+        "title": "New blog",
+        "excerpt":"Recently, I am thinking about ways to improve my English writing skill so I decided to start blogging to share my experience as a software engineer. A developer blog is something that many other software engineers recommend to have if you are a software engineer/developer since it’s a way to...","categories": [],
+        "tags": [],
+        "url": "singmak.github.io/new-blog/"
+      }]
