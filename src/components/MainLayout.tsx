2b8d17e58c39de26450972088084525d5788af10
Sing Mak <mssjsg.singmak@gmail.com>
2022-02-02 22:23:54 +0800

    fix styles in blog post page

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -1,14 +1,15 @@
+import { Box } from '@mui/material';
 import CssBaseline from '@mui/material/CssBaseline';
 import * as React from 'react';
 import Header from './Header';
 
 const MainLayout: React.FC<{ pageTitle: string }> = ({ pageTitle, children }) => {
   return (
-    <div>
+    <Box>
       <Header pageTitle={pageTitle} />
       <CssBaseline />
       {children}
-    </div>
+    </Box>
   );
 };
 
