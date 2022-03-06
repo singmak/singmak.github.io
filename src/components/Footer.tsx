69f4871ae05a88e91955b29ec3109137abd13e92
Sing Mak <sing.mak@fleetship.com>
2022-03-06 19:37:43 +0800

    small changes to header and footer, update logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import { Container, Typography, Box } from '@mui/material';
 export const Footer = () => {
   return (
     <Box sx={{ backgroundColor: '#bedbda', borderTop: '1px solid #99c5c2' }}>
-      <Container maxWidth="md" >
+      <Container>
         <Typography sx={{ textAlign: 'left', paddingTop: 2, paddingBottom: 1, fontSize: '12px' }}>© 2022 Sing Mak</Typography>
       </Container>
     </Box>
