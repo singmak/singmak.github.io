61208fec32482af6213e0627d3a0b8b054d2dc78
Sing Mak <sing.mak@fleetship.com>
2022-03-08 23:39:32 +0800

    add page title and favicon

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -1,6 +1,8 @@
 import { Box } from '@mui/material';
 import CssBaseline from '@mui/material/CssBaseline';
 import * as React from 'react';
+import { Helmet } from 'react-helmet';
+
 import { Footer } from './Footer';
 import Header from './Header';
 import '../styles.css';
@@ -10,6 +12,10 @@ const MainLayout: React.FC<{ pageTitle: string }> = ({ pageTitle, children }) =>
     <Box sx={{
       backgroundColor: '#e6f1f3'
     }}>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Sing Mak&apos;s software engineering journey | {pageTitle}</title>
+      </Helmet>
       <Header pageTitle={pageTitle} />
       <CssBaseline />
       {children}
