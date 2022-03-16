4efc682a58154676490da49ea32ab2c1425aebd5
Sing Mak <mssjsg.singmak@gmail.com>
2022-03-17 00:12:32 +0800

    fix deployment with firebase

--- src/analytics.ts
+++ src/analytics.ts
@@ -1,16 +1,25 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { initializeApp } from 'firebase/app';
 import { getAnalytics, logEvent } from 'firebase/analytics';
 
 import { firebaseConfig } from './firebase-config';
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+let analytics: any = null;
+
+const initFirebase = () => {
+  if (typeof window !== 'undefined' && !analytics) {
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    analytics = getAnalytics(app);
+  }
+};
 
 export const logPage = (componentName: string, pageName: string) => {
+  initFirebase();
   logEvent(analytics, 'screen_view', {
     firebase_screen: pageName, 
     firebase_screen_class: componentName
   });
 };
+
 
