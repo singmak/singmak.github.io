469105d0642f7b0c47d4107f8e8640a7452e201d
Sing Mak <sing.mak@fleetship.com>
2022-03-24 23:53:46 +0800

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Container, Typography, Box } from '@mui/material';
+import { Link } from 'gatsby-theme-material-ui';
 
 export const Footer = () => {
   return (
     <Box sx={{ backgroundColor: '#bedbda', borderTop: '1px solid #99c5c2' }}>
-      <Container>
-        <Typography sx={{ textAlign: 'left', paddingTop: 2, paddingBottom: 1, fontSize: '12px' }}>© 2022 Sing Mak</Typography>
+      <Container sx={{ display: 'flex', textAlign: 'left', paddingTop: 2, paddingBottom: 1 }}>
+        <Typography paragraph sx={{ fontSize: '12px' }}>
+          © 2022 Sing Mak
+        </Typography>
+        <Typography paragraph sx={{ fontSize: '12px', marginLeft: 3 }}>
+        Built with <Link to="https://www.gatsbyjs.com">Gatsby</Link> and <Link to="https://mui.com">MUI</Link>
+        </Typography>
       </Container>
     </Box>
     
