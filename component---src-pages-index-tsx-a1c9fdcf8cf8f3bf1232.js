a7c91b35b55f683e7fd4e49a529e00f58ad225a3
Sing Mak <mssjsg.singmak@gmail.com>
2024-04-14 10:55:41 +0800

    Updates

---
+++ component---src-pages-index-tsx-a1c9fdcf8cf8f3bf1232.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksingsdev=self.webpackChunksingsdev||[]).push([[691],{7200:function(e,t,n){n.r(t);var a=n(7294),s=n(5715),i=n(7735),r=n(6089);t.default=e=>{let{data:t}=e;return(0,a.useEffect)((()=>(0,s.z)("Index","index")),[]),a.createElement(i.Z,{pageTitle:"Blog",description:PAGE_DESCRIPTION},a.createElement(r.Z,{maxWidth:"md",sx:{marginTop:5,marginBottom:5}}))}}}]);
+//# sourceMappingURL=component---src-pages-index-tsx-a1c9fdcf8cf8f3bf1232.js.map
---
+++ component---src-pages-index-tsx-b0cc8f47ca493c1f60d0.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksingsdev=self.webpackChunksingsdev||[]).push([[691],{7200:function(e,t,n){n.r(t);var a=n(7294),s=n(5715),i=n(7735),r=n(6089);t.default=e=>{let{data:t}=e;return(0,a.useEffect)((()=>(0,s.z)("Index","index")),[]),a.createElement(i.Z,{pageTitle:"Blog",description:"Home"},a.createElement(r.Z,{maxWidth:"md",sx:{marginTop:5,marginBottom:5}}))}}}]);
+//# sourceMappingURL=component---src-pages-index-tsx-b0cc8f47ca493c1f60d0.js.map
